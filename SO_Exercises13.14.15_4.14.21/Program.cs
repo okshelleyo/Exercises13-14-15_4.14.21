4e5246b84f2819489c9b6564e52e59aa99d3d418
okshelleyo <oconorshe012@gmail.com>
2021-04-13 21:06:46 -0400

    Exercises 13-15, first entry

---
+++ SO_Exercises13.14.15_4.14.21/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace SO_Exercises13._14._15_4._14._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//Exercise 13: Prompt user for a number. Use a for loop to output all the numbers from that number to 0.
+
+            bool keepLoop = true;
+
+            while (keepLoop)
+            {
+
+                Console.WriteLine("Please give me a number!");
+                string userInput = Console.ReadLine();
+
+                int number = int.Parse(userInput);
+
+                for (int i = number; i >= 0; i--)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine("Would you like to play again? y/n");
+                string userContinue = Console.ReadLine();
+
+                if (userContinue == "y")
+                {
+                    keepLoop = true;
+                }
+                else
+                {
+                    Console.WriteLine("bye!");
+                    break;
+                }
+
+
+            } */
+
+           /*// Exercise 14: Prompt user for a number. Use a for loop to output the squares of all the numbers from 1 to that number.
+
+            bool keepLoop = true;
+
+            while (keepLoop)
+            {
+
+                Console.WriteLine("Please give me a number!");
+                string userInput = Console.ReadLine();
+
+                int number = int.Parse(userInput);
+                int square;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    square = i * i;
+                    Console.WriteLine(square);
+                }
+
+                Console.WriteLine("Would you like to play again? y/n");
+                string userContinue = Console.ReadLine();
+
+                if (userContinue == "y")
+                {
+                    keepLoop = true;
+                }
+                else
+                {
+                    Console.WriteLine("bye!");
+                    break;
+                }
+
+            } */
+
+
+             //Exercise 15: Prompt user for a number. Use a for loop to output the cubes of all the numbers from that number to 0.
+
+             bool keepLoop = true;
+
+             while (keepLoop)
+             {
+
+                 Console.WriteLine("Please give me a number!");
+                 string userInput = Console.ReadLine();
+
+                 int number = int.Parse(userInput);
+                 int cube;
+
+                 for (int i = 1; i <= number; i++)
+                 {
+                     cube = i * i * i;
+                     Console.WriteLine(cube);
+                 }
+
+                 Console.WriteLine("Would you like to play again? y/n");
+                 string userContinue = Console.ReadLine();
+
+                 if (userContinue == "y")
+                 {
+                     keepLoop = true;
+                 }
+                 else
+                 {
+                     Console.WriteLine("bye!");
+                     break;
+                 }
+
+             }  
+        }
+    }
+}
